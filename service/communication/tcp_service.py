f8c7c5228c59303680a53b059e5bd3b446b98d3f
Frederik Neitzel <frederik.neitzel@haw-hamburg.de>
2024-11-04 18:42:04 +0100

    version 1. Praktikum

---
+++ service/communication/tcp_service.py
@@ -0,0 +1,32 @@
+import requests
+
+#send tcp Methode request, type, endpoint, body
+def send_tcp_request(method, url, body, type):
+    try:
+        response = requests.request(method, url, data=body, headers={'Content-Type': type})
+        if response.status_code == 200:
+            return response.json()
+    except Exception as e:
+        print(e)
+
+
+def register_component_in_sol(self, sol_ip, sol_tcp, star, sol, com_uuid, ip, port, status):
+    sol_url = f"http://{sol_ip}:{sol_tcp}/vs/v1/system/"
+    # format data as text/plain
+    post_data = (
+        f"star: {star}\n"
+        f"sol: {sol}\n"
+        f"component: {com_uuid}\n"
+        f"com-ip: {ip}\n"
+        f"com-tcp: {port}\n"
+        f"status: {status}"
+    )
+    try:
+        post_response = requests.post(sol_url, data=post_data, headers={
+            'Content-Type': 'text/plain'})
+        return post_response.status_code
+    except requests.Timeout:
+        print(f"Timeout occurred while trying to reach {sol_url}")
+        return None
+
+#need to listen for response from SOL
