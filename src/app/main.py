8afb10d45c9341c786584c5b5ab5b7c165b287a7
LukasLBRCHT <elster1102@gmail.com>
2024-11-26 10:43:30 +0100

    added input reader

--- src/app/main.py
+++ src/app/main.py
@@ -8,6 +8,7 @@ from src.model.peer import Component
 from src.utils.uuid_generator import UuidGenerator
 from src.service.peer_service import PeerService
 from src.utils.logger import Logger
+from src.service.input_reader import Input_Reader
 
 
 """
@@ -20,7 +21,7 @@ app = Flask(__name__)
 
 # Function to run Flask app
 def run_flask():
-    app.run(host=Config.IP, port=Config.STAR_PORT, debug=True)
+    app.run(host=Config.IP, port=9090, debug=True)
 
 # Start Flask in a separate thread
 flask_thread = threading.Thread(target=run_flask)
@@ -37,5 +38,10 @@ peer = Component(Config.IP, Config.PEER_PORT, UuidGenerator.generate_com_uuid())
 peer_service = PeerService(peer, logger)
 
 peer_manager = PeerManager(peerService=peer_service)
+
+#start input-reader-thread
+reader = Input_Reader(peer_service, peer)
+reader_thread = threading.Thread(target=reader.read_input)
+reader_thread.start()
 
 peer_manager.manage() #Startet Peer-Manager
