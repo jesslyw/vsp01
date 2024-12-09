1eb55a15b194aa75f7da55a15abb666e3f8a200d
TheNinaNiesche <nina.niesche@gmx.de>
2024-12-09 13:56:04 +0100

    - Better error-responses and cleaner code in controller.py - moved registered_peers to model - removed peer_controller.py - removed sol_controller.py

--- src/model/sol.py
+++ src/model/sol.py
@@ -1,4 +1,6 @@
 # Definiert die Star-Klasse, die den Stern und seine Eigenschaften beschreibt.
+from threading import Lock
+
 
 class SOL:
     def __init__(self, com_uuid, star_uuid):
@@ -10,3 +12,18 @@ class SOL:
         self.num_active_components = None
         self.max_active_components = None
         self.sol_initialized_at = None
+        self.registered_peers = []
+        self.peers_lock = Lock()
+
+    def add_peer(self, peer):
+        with self.peers_lock:
+            self.registered_peers.append(peer)
+
+    def remove_peer(self, peer):
+        with self.peers_lock:
+            self.registered_peers.remove(peer)
+
+    def find_peer(self, com_uuid):
+        with self.peers_lock:
+            return next((p for p in self.registered_peers if p.com_uuid == com_uuid), None)
+
