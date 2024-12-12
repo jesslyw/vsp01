deb53158de744a7ee973b51fcb9332224576c5d9
Lumikso <jessicalim@this-is-not-a-mac-2.local>
2024-12-12 22:20:43 +0100

    fix: add correct time intervals for retries and small corrections

--- src/app/config.py
+++ src/app/config.py
@@ -9,12 +9,12 @@ class Config:
     IP = socket.gethostbyname(socket.gethostname())
     API_BASE_URL = "/vs/v1/system/"
 
-    #UDP-Konfiguration
-    TIMEOUT_LISTENING_FOR_UPD_RESPONSE= 5 # in Sekunden
+    # UDP-Konfiguration
+    TIMEOUT_LISTENING_FOR_UPD_RESPONSE = 5  # in Sekunden
 
     # Broadcast-Konfiguration
     BROADCAST_INTERVAL = 5  # Interval in Sekunden für Broadcasts
-    BROADCAST_RETRY_ATTEMPTS = 2 # Anzahl der Widerholungen für HALLO? 
+    BROADCAST_RETRY_ATTEMPTS = 2  # Anzahl der Widerholungen für HALLO?
 
     # Stern- und Komponenten-Konfiguration
     MAX_COMPONENTS = 4  # Maximale Anzahl an Komponenten in einem Stern
@@ -24,14 +24,16 @@ class Config:
     # Status Update Konfiguration
     STATUS_UPDATE_INTERVAL = 30  # Intervall in Sekunden zwischen Statusmeldungen
     STATUS_UPDATE_RETRY_INTERVALS = [10, 20]  # Retry-Intervalle in Sekunden
-    STATUS_UPDATE_MAX_ATTEMPTS = 1  # Maximale Wiederholungsversuche
+    STATUS_UPDATE_MAX_ATTEMPTS = 3  # Maximale Wiederholungsversuche
 
     # Timeout-Konfiguration
     STATUS_UPDATE_TIMEOUT = 5  # Timeout für die HTTP-Requests in Sekunden
 
     # Exit-Request-Konfiguration
     EXIT_REQUEST_RETRIES = 2  # Anzahl der Wiederholungen für Exit-Requests
-    EXIT_REQUEST_WAIT = [10, 20]  # Wartezeit zwischen Exit-Request-Versuchen in Sekunden
+    # fmt: off
+    EXIT_REQUEST_WAIT = [10, 20] # Wartezeit zwischen Exit-Request-Versuchen in Sekunden
+    # fmt: on
 
     # Gesundheitsprüfung
     HEALTH_CHECK_INTERVAL = 30  # Intervall in Sekunden für die Gesundheitsprüfung
@@ -39,7 +41,7 @@ class Config:
 
     # Abmeldeversuche
     UNREGISTER_RETRY_COUNT = 2  # Anzahl der Wiederholungen für Abmeldeversuche
-    UNREGISTER_RETRY_DELAY = 10  # Verzögerung in Sekunden zwischen Wiederholungen
+    UNREGISTER_RETRY_DELAY = [10, 20]  # Verzögerung in Sekunden zwischen Wiederholungen
 
     # API-Felder
     STAR_UUID_FIELD = "star"
@@ -56,7 +58,8 @@ class Config:
     LOGGING_LEVEL = "INFO"
 
     # Exit-Konfiguration
-    SOL_EXIT_CODE = 0  # Exit-Code für SOL
+    EXIT_CODE_SUCCESS = 0  # Successful exit
+    EXIT_CODE_ERROR = 1  # Exit with error
 
     # Syslog logging (/var/log/syslog) anschalten (false für lokale Enwicklung)
     LOG_TO_SYSLOG = False
